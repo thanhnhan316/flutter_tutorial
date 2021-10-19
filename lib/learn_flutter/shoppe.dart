4fcb1e1584eb7905afc1496202b39c2ea4a3156f
thanhnhan316 <80688564+thanhnhan316@users.noreply.github.com>
2021-10-19 22:59:56 +0700

    hahahha

--- lib/learn_flutter/shoppe.dart
+++ lib/learn_flutter/shoppe.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hello_world/learn_flutter/callfakeapi.dart';
 import 'package:http/http.dart' as http;
 
 class ScreenShoppe extends StatelessWidget {
@@ -23,6 +24,7 @@ class BuildShoppe extends StatefulWidget {
 }
 
 class _BuildShoppeState extends State<BuildShoppe> {
+  late Size size;
   @override
   void initState() {
     // TODO: implement initState
@@ -32,6 +34,7 @@ class _BuildShoppeState extends State<BuildShoppe> {
 
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
     return Scaffold(
       body: buildBody(),
     );
@@ -43,24 +46,13 @@ class _BuildShoppeState extends State<BuildShoppe> {
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
             var data = snapshot.data;
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (BuildContext context, int index) {
+            return GridView.count(
+              padding: EdgeInsets.all(7),
+              crossAxisCount: 2,
+              children: List.generate(data.length, (index) {
                 Product p = data[index];
-                return Card(
-                  child: Column(
-                    children: [
-                      Text("${p.id}"),
-                      Text(p.title),
-                      Text(p.description),
-                      Text(p.category),
-                      Text("${p.price}"),
-                      Image.network(p.imgge),
-                      SizedBox(height: 20)
-                    ],
-                  ),
-                );
-              },
+                return buildItem(p);
+              }),
             );
           }
           return Center(
@@ -91,6 +83,96 @@ class _BuildShoppeState extends State<BuildShoppe> {
       }
     }
     return ls;
+  }
+
+  Widget buildItem(Product p) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Image.network(p.imgge,
+                height: size.height * 0.135,
+                width: size.width * 0.27,
+                fit: BoxFit.fill),
+            Text(
+              p.title,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "${p.price}\$",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Colors.amber[700],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Mua hàng ngay'),
+                        content: const Text('».·º`·.LOVE.·´º·.« '),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text('Để sau'),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text('Mua'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding:
+                        EdgeInsets.only(left: 6, right: 6, top: 2, bottom: 2),
+                    child: Text("Mua ngay",
+                        style: TextStyle(fontSize: 14, color: Colors.white)),
+                    decoration: BoxDecoration(
+                        color: Colors.amber[700],
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+                GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Thêm vào giỏ hàng'),
+                          content: const Text('Thêm vào nào hihihi (-_-)'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('Để sau'),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('Thêm'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.amber[700],
+                      size: 20,
+                    ))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
